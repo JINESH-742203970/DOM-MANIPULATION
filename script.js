87f772f7b6568331f3d9ad29e5b977e74732636a
JINESH-742203970 <103113331+JINESH-742203970@users.noreply.github.com>
2023-03-12 01:43:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const btnL = document.getElementById("btnL");
+const btnR = document.getElementById("btnR");
+const data = document.getElementById("data");
+const reset = document.getElementById("reset");
+btnL.addEventListener("click", () => {
+    const value = Number(data.innerText);
+    console.log('Y');
+    if (value > 0)
+        data.innerText = value - 1;
+    else
+        alert("Negative value not allowed")
+
+})
+btnR.addEventListener("click", () => {
+    const value = Number(data.innerText);
+    console.log("H");
+    if (value >= 10)
+        alert("10+ values are not allowed");
+    else
+        data.innerText = value + 1;
+})
+reset.addEventListener("click", () => {
+    data.innerText = 0;
+})
